82c2532aa3a75bf7b5c0c5efc59cfacdce79be7d
qiyoudaoyi <qiyoudaoyi@gmail.com>
2021-10-03 19:30:34 +0800

    p2 2 comments

--- practice2/TAs_Heartfelt.c
+++ practice2/TAs_Heartfelt.c
@@ -2,6 +2,36 @@
 
 int main() 
 {
+    /****************************************************/
+    /*  float is stored as follows:                     */
+    /*  float: |01000010|00000001|01000111|10101110|    */
+    /*                                                  */
+    /*  when we do p = (int*)&x,                        */
+    /*  p is an int pointer that points to the float    */
+    /*  that points to the previous float;              */
+    /*  however, p is now an int pointer:               */
+    /*  p ->   |01000010|00000001|01000111|10101110|    */
+    /*                                                  */
+    /*  1 (an int) in the memory look like:             */
+    /*  1:     |00000000|00000000|00000000|00000001|    */
+    /*                                                  */
+    /*  1 << i  (bitwise) shifts i bits in the memory:  */
+    /*  For examples,                                   */
+    /*  1<<1 : |00000000|00000000|00000000|00000001|    */
+    /*  1<<2 : |00000000|00000000|00000000|00000010|    */
+    /*   ...      ...       ...      ...     ...        */
+    /*  1<<31: |10000000|00000000|00000000|00000000|    */
+    /*                                                  */
+    /*  so (*p & 1 << i ) is true only if               */
+    /*  the i-nd bit of *p and 1<<2 are both 1,         */
+    /*                                                  */
+    /*  For examples,                                   */
+    /*  p ->   |01000010|00000001|01000111|10101110|    */
+    /*  1<<2:  |10000000|00000000|00000000|00000010|    */
+    /*  the 2nd bit of *p and 1<<2 are both 1,          */
+    /*  and therefore (*p & 1 << 2 ) is true            */
+    /****************************************************/
+
     float x;
 
     // we need an int* because only int* can perform bitwise operation

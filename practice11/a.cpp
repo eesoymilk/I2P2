ac3eb6573607177eaa4be8d76cefe57e170a4eee
qiyoudaoyi <qiyoudaoyi@gmail.com>
2021-12-24 17:55:50 +0800

    p11q1

--- practice11/a.cpp
+++ practice11/a.cpp
@@ -6,20 +6,23 @@
 #include <string>
 using namespace std;
 
-set<char> VOWEL = {'a', 'e', 'i', 'o', 'u'};
+const set<char> VOWEL = {'a', 'e', 'i', 'o', 'u'};
+
+// Possible # of first/second words in both lines of the lyric
+int PerfectMatch = 0, VowelCntMatch[201] = {0};
 
 class WORD
 {
 private:
-    int cnt_v = 0;
-    char last_v = '\0';
 public:
-    WORD(string& word)
+    int cnt_v = 0;
+    char lst_v = '\0';
+    WORD(const string& word)
     {
         for (auto c : word) {
             if (VOWEL.find(c) != VOWEL.end()) {
                 cnt_v++;
-                last_v = c;
+                lst_v = c;
             }
         }
     }
@@ -33,16 +36,39 @@ public:
 
 int main()
 {
-    int n;
+    int n, res;
     string buf;
+    map<WORD, int> dict;
 
     cin >> n;
-
     for (int i = 0; i < n; i++) {
         cin >> buf;
-        // WORD word(buf);
-        cout << "hi\n";
+        WORD word(buf);
+        if (dict.find(word) != dict.end()) {
+            dict.insert(pair<WORD, int>(word, 1));
+        } else {
+            dict[word]++;
+        }
     }
+
+    // count maximum numbers of possible sw;
+    for (auto iter = dict.begin(); iter != dict.end(); iter++) {
+        if (iter->second >= 2)
+            PerfectMatch += iter->second / 2;
+        if (iter->second %= 2)
+            VowelCntMatch[(iter->first).cnt_v]++;
+    }
+    // Greedy matching
+    for (auto recur : VowelCntMatch) {
+        recur /= 2;
+        while (PerfectMatch && recur) {
+            PerfectMatch--;
+            recur--;
+            res++;
+        }
+    }
+    res += PerfectMatch / 2;
+    cout << res << endl;
 
     return 0;
 }
